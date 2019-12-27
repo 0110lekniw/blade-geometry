92d9e95735549dc1f969c5a6f545df17e6878b1e
Adrian <chojnacki.adrian@yahoo.com>
2019-12-27 17:16:13 +0100

    Correction of new functions

--- methods/geometrical_calculations.py
+++ methods/geometrical_calculations.py
@@ -2,20 +2,26 @@ import numpy as np
 import math
 from methods.equations_solver import linearCramerRoots
 
+# Calculation of linear function equation from two points
 def twoPointsLinearFunction(point_one, point_two):
-    a = (point_two[1] - point_one[1]) / (point_two[0] - point_one[0])
-    b = 0 + point_one[1] - point_one[0] * a
-    linear = np.poly1d([a, b])
-    return linear
+    if point_one[0] == point_two[0]:
+        print('no function')
+        return 'non_func'
+    else:
+        a = (point_two[1] - point_one[1]) / (point_two[0] - point_one[0])
+        b = 0 + point_one[1] - point_one[0] * a
+        linear = np.poly1d([a, b])
+        return linear
 
-
+# Calculation of linear functions intersection point
 def linearFunctionsIntersections(function_one, function_two):
     x = (function_two[0]-function_one[0])/(function_two[1]-function_one[1])
     return np.array([x, function_two(x)])
 
-def vectorCalculation(first_point, second_point):
-    delta_x = second_point[0] - first_point[0]
-    delta_y = second_point[1] - first_point[1]
+# Calculation of vector matrix
+def vectorCalculation(terminal_point, initial_point):
+    delta_x = terminal_point[0] - initial_point[0]
+    delta_y = terminal_point[1] - initial_point[1]
     modulo = math.sqrt(delta_x**2+delta_y**2)
     return np.array([delta_x, delta_y, modulo])
 
@@ -32,7 +38,7 @@ def degreeBetweenVectors(vector_1, vector_2):
     return degree
 
 
-def averageDegreeBiscetorEquation(linear_one, linear_two, which_one):
+def averageDegreeBisectorEquation(linear_one, linear_two, which_one):
     # dc  - directional coefficient
     bisector_dc_smaller_angle = math.tan((math.atan(linear_one[1]) + math.atan(linear_two[1])) / 2)
     intersection_point = linearFunctionsIntersections(linear_one, linear_two)
@@ -60,19 +66,37 @@ def averageDegreeBiscetorEquation(linear_one, linear_two, which_one):
     else:
         return larger_angle_bisector
 
+
 def movePointsByVector(points, vector):
+    new_points = np.zeros((points.shape[0], points.shape[1]))
     for i in range(points.shape[0]):
-        points[i , 0] += vector[0]
-        points[i , 1] += vector[1]
-    return points
+        new_points[i, 0] = points[i, 0] + vector[0]
+        new_points[i, 1] = points[i, 1] + vector[1]
+    return new_points
+
 
 def turnPoint(points, angle):
+    new_points = np.zeros((points.shape[0], points.shape[1]))
     for i in range(points.shape[0]):
         x = points[i, 0]
-        y = points[i, 0]
-        points[i, 0] = (x*math.cos(angle)-y*math.sin(angle))
-        points[i, 1] = (x*math.sin(angle)+y*math.cos(angle))
-    return points
+        y = points[i, 1]
+        new_points[i, 0] = (x*math.cos(angle)-y*math.sin(angle))
+        new_points[i, 1] = (x*math.sin(angle)+y*math.cos(angle))
+    return new_points
+
+
+def turnAroundPoint(points, degree, center_point):
+    vector_of_move = vectorCalculation(np.array([0, 0]), center_point)
+    moved_points = movePointsByVector(points, vector_of_move)
+    turned_points = turnPoint(moved_points, degree)
+    vector_of_move = np.multiply(vector_of_move, (-1))
+    expected_coordinates = movePointsByVector(turned_points, vector_of_move)
+    return expected_coordinates
+
+
+
+
+
 
 
 
--- testing/test.py
+++ testing/test.py
@@ -1,16 +1,31 @@
 import methods.geometrical_calculations as gc
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 first_point = np.array([[0, 0], [1, 2], [2, 4]])
-second_point = np.array([[0, 0], [1, 1], [2, 2]])
+second_point = np.array([[-1, 10], [1, -10], [2, -20]])
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 10))
 linear_function_1 = gc.twoPointsLinearFunction(first_point[0, :], first_point[1, :])
 linear_function_2 = gc.twoPointsLinearFunction(second_point[0, :], second_point[1, :])
 x = np.linspace(-10, 10, 100)
-plt.plot(x, linear_function_1(x), '-', x, linear_function_2(x), '--')
+plt.plot(x, linear_function_1(x), '+', x, linear_function_2(x), '+')
 
-bisectors = gc.averageDegreeBiscetorEquation(linear_function_1, linear_function_2, 'larger')
-plt.plot(x, bisectors(x), color='b')
+# bisectors = gc.averageDegreeBisectorEquation(linear_function_1, linear_function_2, 'larger')
+# plt.plot(x, bisectors(x), color='b')
+
+degree = math.pi/2
+first_points_turned = gc.turnAroundPoint(first_point, degree, np.array([0, 0]))
+second_points_turned = gc.turnAroundPoint(second_point, degree, np.array([0, 0]))
+linear_function_3 = gc.twoPointsLinearFunction(first_points_turned[0, :], first_points_turned[1, :])
+linear_function_4 = gc.twoPointsLinearFunction(second_points_turned[0, :], second_points_turned[1, :])
+
+plt.plot(x, np.zeros((x.shape[0],1)), color='k')
+plt.plot(np.zeros((x.shape[0],1)), x, color='k')
+plt.plot(x, linear_function_3(x), '-', x, linear_function_4(x), '-')
+plt.scatter(first_point[:, 0], first_point[:, 1], color="k")
+plt.scatter(first_points_turned[:, 0], first_points_turned[:, 1], color="g")
+plt.scatter(second_point[:, 0], second_point[:, 1], color="r")
+plt.scatter(second_points_turned[:, 0], second_points_turned[:, 1], color="y")
 plt.show()
